ded5596f9ae78669f3fdfa913875b6367ba8e870
Samuele <44635579+wiztoth@users.noreply.github.com>
2020-05-11 13:32:35 +0200

    credentials cleaning

--- auth.py
+++ auth.py
@@ -1,14 +1,14 @@
-token = "NzA1MDQ2ODczOTE1Nzg1Mjc3.XriZPg.u3ycAT23jwYnQ_QDFtg4L8zbGo0"
+token = "YOUR TOKEN FOR DISCORD BOT"
 
-db_host = 'localhost'
+db_host = 'YOUR DATABASE HOST'
 #spells credentials
-db_user_spell = 'spellbook_user'
-db_psw_spell = 'CastingFireBall'
-db_name_spell = 'dnd_5_spells'
+db_user_spell = 'YOUR DATABASE SPELL USERNAME'
+db_psw_spell = 'YOUR DATABASE SPELL PASSWORD'
+db_name_spell = 'YOUR DATABASE SPELLS NAME'
 #weapons credentials
-db_user_weapons = 'weapons_user'
-db_psw_weapons = 'GetAllWeapons'
-db_name_weapons = 'dnd_5_weapons'
+db_user_weapons = 'YOUR DATABASE WEAPONS USERNAME'
+db_psw_weapons = 'YOUR DATABASE WEAPONS PASSWORD'
+db_name_weapons = 'YOUR DATABASE WEAPONS NAME'
 
 
 
